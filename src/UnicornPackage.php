0c0469ccf1084cb6ec32e644ba95475ff36b932d
Simon Asika <asika32764@gmail.com>
2021-03-27 02:07:51 +0800

    package

---
+++ src/UnicornPackage.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Part of starter project.
+ *
+ * @copyright  Copyright (C) 2021 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+namespace Unicorn;
+
+use Windwalker\Core\Package\AbstractPackage;
+
+/**
+ * The UnicornPackage class.
+ */
+class UnicornPackage extends AbstractPackage
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function install(): void
+    {
+    }
+}
