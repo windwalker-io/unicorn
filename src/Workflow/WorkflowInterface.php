741b32b95333706ad5e6c061fe74779e58b79e19
Simon Asika <asika32764@gmail.com>
2021-08-15 02:42:44 +0800

    New workflow wip

---
+++ src/Workflow/WorkflowInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Part of unicorn project.
+ *
+ * @copyright  Copyright (C) 2021 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+namespace Unicorn\Workflow;
+
+/**
+ * Trait WorkflowInterface
+ */
+trait WorkflowInterface
+{
+}
