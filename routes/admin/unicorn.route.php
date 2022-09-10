b2a437fb321793f432089d4870b1f4bb5afa763b
Simon Asika <asika32764@gmail.com>
2022-09-11 03:12:42 +0800

    FileController::upload add default access control #328

--- routes/admin/unicorn.route.php
+++ routes/admin/unicorn.route.php
@@ -11,13 +11,14 @@ declare(strict_types=1);
 
 namespace App\Routes;
 
+use Lyrasoft\Luna\Middleware\LoginRequireMiddleware;
 use Unicorn\Module\FileUpload\FileController;
 use Windwalker\Core\Middleware\JsonApiMiddleware;
-use Windwalker\Core\Middleware\JsonResponseMiddleware;
 use Windwalker\Core\Router\RouteCreator;
 
 /** @var RouteCreator $router */
 
 $router->post('file_upload', '/file/upload')
     ->controller(FileController::class, 'upload')
+    ->middleware(LoginRequireMiddleware::class)
     ->middleware(JsonApiMiddleware::class);
