d55d0bbd4161814e187d5bb8738bbb63ae581c1e
Simon Asika <asika32764@gmail.com>
2023-04-23 03:32:45 +0800

    Add AJAX attribute or trait for controller #420

---
+++ src/Controller/AjaxControllerTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Part of earth project.
+ *
+ * @copyright  Copyright (C) 2023 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+namespace Unicorn\Controller;
+
+use Windwalker\Core\Application\AppContext;
+use Windwalker\Core\Attributes\JsonApi;
+
+/**
+ * Trait AjaxControllerTrait
+ */
+trait AjaxControllerTrait
+{
+    #[JsonApi]
+    public function ajax(AppContext $app): mixed
+    {
+        $task = $app->input('task');
+
+        return $app->call([$this, $task]);
+    }
+}
