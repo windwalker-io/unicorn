de59a9b84a56187198ea4c3b37ed23ff4624222c
Simon Asika <asika32764@gmail.com>
2023-06-23 19:14:20 +0800

    Ajax attribute

---
+++ src/Attributes/Ajax.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Part of earth project.
+ *
+ * @copyright  Copyright (C) 2023 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+namespace Unicorn\Attributes;
+
+/**
+ * The Ajax class.
+ */
+#[\Attribute(\Attribute::TARGET_METHOD | \Attribute::TARGET_FUNCTION)]
+class Ajax
+{
+    //
+}
--- src/Controller/AjaxControllerTrait.php
+++ src/Controller/AjaxControllerTrait.php
@@ -11,8 +11,11 @@ declare(strict_types=1);
 
 namespace Unicorn\Controller;
 
+use Unicorn\Attributes\Ajax;
+use Windwalker\Attributes\AttributesAccessor;
 use Windwalker\Core\Application\AppContext;
 use Windwalker\Core\Attributes\JsonApi;
+use Windwalker\Core\Router\Exception\RouteNotFoundException;
 
 /**
  * Trait AjaxControllerTrait
@@ -22,8 +25,24 @@ trait AjaxControllerTrait
     #[JsonApi]
     public function ajax(AppContext $app): mixed
     {
-        $task = $app->input('task');
+        $task = $app->input('task') ?? 'index';
 
-        return $app->call([$this, $task]);
+        if (!method_exists($this, $task)) {
+            throw new RouteNotFoundException('Action not found.');
+        }
+
+        $callable = [$this, $task];
+
+        $ajaxAttr = AttributesAccessor::getFirstAttribute($callable, Ajax::class);
+
+        if (!$ajaxAttr) {
+            throw new RouteNotFoundException(
+                WINDWALKER_DEBUG
+                    ? "$task() is not an AJAX method, try add #[Ajax] to method"
+                    : 'Action not found.'
+            );
+        }
+
+        return $app->call($callable);
     }
 }
