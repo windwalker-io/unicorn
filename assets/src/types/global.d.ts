5e8f94e52453921a8148a0120c201ac2c81cad8d
Simon Asika <asika32764@gmail.com>
2023-11-12 15:12:54 +0800

    types

---
+++ assets/src/types/global.d.ts
@@ -0,0 +1,12 @@
+import '../index';
+import 'systemjs';
+import 'jquery';
+import 'axios';
+import boostrap from 'bootstrap';
+import * as vue from 'vue';
+
+declare global {
+  var S:  typeof System;
+  var bootstrap: typeof boostrap;
+  var Vue: typeof vue;
+}
